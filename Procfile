547861e785605b108be3bc0b68d27db3408d16d4
Kacper Myslinski <kacpermyslinski@gmail.com>
2016-10-21 18:18:54 -0500

    profile change

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python dealscraper/main.py --log-file -
+web: python main.py --log-file -
--- main.py
+++ main.py
@@ -303,6 +303,5 @@ def deleteFishLure(fish_id, lure_id):
 
 
 if __name__ == '__main__':
-	app.secret_key = 'so_secret_key'
-	app.debug = False
-	app.run(host='0.0.0.0', port=33507)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
